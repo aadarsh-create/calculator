bd861d6c8aad1b488ac0509427fcee4afc991967
aadarsh-create <p.aadarsh2006@gmail.com>
2026-05-24 18:42:53 +0530

    complete script js

---
+++ script.js
@@ -0,0 +1,23 @@
+const display = document.getElementById("screen");
+function addexp(exp) {
+  if (display.innerText === "Error") {
+    display.innerText = "";
+  }
+  display.innerText += exp;
+}
+
+function result() {
+  try {
+    display.innerText = eval(display.innerText);
+  } catch {
+    display.innerText = "Error";
+  }
+}
+
+function clr() {
+  display.innerText = "";
+}
+
+function bckspc() {
+  display.innerText = display.innerText.slice(0, -1);
+}
